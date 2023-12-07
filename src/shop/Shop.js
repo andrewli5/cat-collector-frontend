58cb9dc49764b659ac8f533c14603163bf9deaa9
vanechaaale <vanessal2001@gmail.com>
2023-12-07 02:34:31 -0500

    add borders to shop

--- src/shop/Shop.js
+++ src/shop/Shop.js
@@ -1,7 +1,8 @@
-import { Box, Grid, Typography, Item } from "@mui/material";
+import { Box, Grid, Typography, Item, Container } from "@mui/material";
 import { useEffect } from "react";
 import { APP_NAME } from "../constants";
 import ShopItem from "./ShopItem";
+import "../css/styles.css";
 
 export default function Shop() {
   useEffect(() => {
@@ -9,12 +10,37 @@ export default function Shop() {
   }, []);
 
   return (
-    <Box>
-      <Grid container spacing={2}>
-        <ShopItem icon="icon" title="title" description="description" price="price" />
-        <ShopItem icon="test" title="test" description="test" price="test" />
-
+    <Grid className="shop" container spacing={1}>
+      <Grid className="shop" item xs={7}>
+        <Grid container spacing={1} maxHeight={'100vh'} overflow="auto">
+          <ShopItem icon="cat1.png" title="test title." description="test description. i am writing words." name="cat1" price="1,000,000" />
+          <ShopItem icon="cat1.png" title="test title." description="test description. i am writing words." name="cat1" price="1,000,000" />
+        </Grid>
       </Grid>
-    </Box>
+      <Grid className="shop" item xs={5}>
+        <Grid container spacing={1}>
+          <Grid item>
+            icon
+          </Grid>
+          <Grid item xs={2}>
+            title
+          </Grid>
+          <Grid item xs={5}>
+            desc
+          </Grid>
+          <Grid item xs={3}>
+            1,000,000
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid className="shop" item xs={12}>
+        <Grid container>
+          <Grid item xs={12}>
+            <ShopItem icon="cat3.png" title="test title3." description="test description3" name="cat1" price={100} />
+
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
--- src/shop/ShopItem.js
+++ src/shop/ShopItem.js
@@ -3,16 +3,16 @@ import { Grid, Typography } from "@mui/material";
 export default function ShopItem({icon, title, description, price}) {
     return(
         <>
-            <Grid item xs={1} md={1}>
+            <Grid item xs={2} md={2}>
                 <Typography>{icon}</Typography>
             </Grid>
             <Grid item xs={4} md={3}>
                 <Typography>{title}</Typography>
             </Grid>
-            <Grid item xs={6} md={6}>
+            <Grid item xs={4} md={5}>
             <Typography>{description}</Typography>
             </Grid>
-            <Grid item xs={1} md={2}>
+            <Grid item xs={1} md={1}>
                 <Typography>{price}</Typography>
             </Grid>
         </>
