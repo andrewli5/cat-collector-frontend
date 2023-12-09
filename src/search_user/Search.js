8abaeeb2c5fea462b0fd01cee0a65a33445f1644
Joseph Pierre-Louis <pierre-louis.jo@northeastern.edu>
2023-12-09 17:56:00 -0500

    functionality complete

--- src/search_user/Search.js
+++ src/search_user/Search.js
@@ -1,41 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Typography } from "@mui/material";
-import TextField from '@mui/material/TextField';
-import { useNavigate } from 'react-router-dom';
+import TextField from "@mui/material/TextField";
+import { useNavigate } from "react-router-dom";
+import { getUserByUsername } from "../client";
 
 const SearchUser = () => {
-    const [username, setUsername] = useState('search users...'); // Set the initial value here
+    const [username, setUsername] = useState("search users...");
     const navigate = useNavigate();
-
     const handleKeyPress = async (event) => {
-        if (event.key === 'Enter') {}
-            const userExists = await checkUserExists(username);
-            if (userExists) {
-                navigate(`/profile/${username}`);
+        
+        if (event.key === "Enter") {
+            const userExists = await getUserByUsername(username);
+            if (userExists === null) {
+                setUsername("user does not exist :(");
             } else {
-                console.log('User does not exist');
-                // Handle user not found scenario
+                navigate(`/profile/${username}`);
             }
         }
     };
 
-    const checkUserExists = async (username) => {
-        // Make an API call or perform a database query to check if the user exists
-        // Return true if the user exists, false otherwise
+    const handleFocus = () => {
+        if ((username === "search users...") || (username === "user does not exist :(")) {
+            setUsername("");
+        }
     };
 
-
     return (
-        <div style={{ height: '30px' }}>
-            <TextField 
-                size='small'
+        <div style={{ height: "30px" }}>
+            <TextField
+                rows={1}
+                size="small"
                 value={username}
                 onChange={(event) => setUsername(event.target.value)}
                 onKeyPress={handleKeyPress}
+                onFocus={handleFocus}
+                type={"standard"}
+                InputProps={{
+                    style: {
+                        fontSize: "22px",
+                        border: "0px",
+                        fontColor: "gray",
+                    },
+                }}
             />
         </div>
     );
 };
 export default SearchUser;
-
-
