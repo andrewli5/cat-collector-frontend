cacf2ba1afdf308c7f82439ddea9105c568de692
vanechaaale <vanessal2001@gmail.com>
2023-12-25 00:31:47 -0500

    menu icons

--- src/profile/SelectProfilePhoto.js
+++ src/profile/SelectProfilePhoto.js
@@ -1,8 +1,35 @@
 import { useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import { importAll } from "../utils/importAll";
+import Icon from "../assets/cat_face_silhouette.png";
 
 export default function SelectProfilePhoto() {
   const [photo, setPhoto] = useState(null);
+  const icons = importAll(
+    require.context("../assets/catIcons", false, /\.(png|jpe?g|svg)$/)
+  );
 
-  return <Box sx={{ border: "1px solid grey" }}>photos here</Box>;
+  return (
+    <Box>
+      <Grid
+        container
+        sx={{ maxWidth: "50vh", maxHeight: "50vh", overflow: "scroll" }}
+      >
+        {Object.keys(icons).map((icon) => (
+          <Grid item>
+            <Box
+              component="img"
+              src={icons[icon]}
+              sx={{
+                border: "1px solid white",
+                borderRadius: "80px",
+                width: "15vh",
+                height: "15vh",
+              }}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
